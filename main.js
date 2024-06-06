7e5524c9c82759a0ab2a4f5aecf5c3d0aa7a582f
Script Raccoon <scriptraccoon@gmail.com>
2024-06-06 12:36:01 +0200

    check times as well

--- main.js
+++ main.js
@@ -7,67 +7,113 @@ import dotenv from "dotenv"
 dotenv.config()
 
 /**
+ * Berlin service website.
+ */
+const BASE_URL = "https://service.berlin.de"
+
+/**
  * URL for booking a slot in Berlin for getting a new ID card.
  * Replace the /120703 with the ID for the service you want to book.
  * See https://service.berlin.de/terminvereinbarung/.
  */
-const BOOKING_URL = "https://service.berlin.de/terminvereinbarung/termin/all/120703/"
+const BOOKING_URL = `${BASE_URL}/terminvereinbarung/termin/all/120703/`
 
 /**
  * Checks if there are any appointments available for getting a new ID card in Berlin,
  * and if so, sends an email to the recipient email address with a link to the booking page
  */
-async function main() {
-	const browser = await puppeteer.launch()
+async function check_appointments() {
+	try {
+		console.info("Checking for appointments...")
+		const browser = await puppeteer.launch()
 
-	const page = await browser.newPage()
-	await page.goto(BOOKING_URL)
+		const page = await browser.newPage()
+		await page.goto(BOOKING_URL)
 
-	const appointments_available = await has_appointments(page)
+		const appointments_available = await has_appointments(page)
+		if (!appointments_available) {
+			console.info("No appointments are available")
+			await browser.close()
+			return
+		}
 
-	if (appointments_available) {
 		console.info("Appointments are available")
-		const date = await get_date(page)
-		send_email(date)
-	} else {
-		console.info("No appointments are available")
-	}
 
-	await browser.close()
+		const slot = await get_slot(page)
+		if (!slot) {
+			console.info("No slot available")
+			await browser.close()
+			return
+		}
+
+		console.info("Slot is available:", slot.date)
+
+		await page.goto(BASE_URL + slot.url)
+		const time = await get_time(page)
+		if (!time) {
+			console.info("No time available")
+			await browser.close()
+			return
+		}
+
+		console.info("Time is available:", time)
+
+		await send_email(slot.date, time)
+		await browser.close()
+	} catch (error) {
+		console.error(error)
+	}
 }
 
 /**
- * Checks if there are any appointments available for getting a new ID card in Berlin
- * @param {import("puppeteer").Page} page
+ * Checks if there are any appointments available for getting a new ID card in Berlin.
+ * @param {import("puppeteer").Page} page - The page to check for appointments.
  * @returns {Promise<boolean>}
  */
 async function has_appointments(page) {
-	const body = await page.$("body")
-	if (!body) return false
-	const txt = await page.evaluate((body) => body.innerText, body)
+	const txt = await page.evaluate(() => document.body.innerText)
 	return txt.includes("Bitte wählen Sie ein Datum")
 }
 
 /**
- * Gets the date for which the appointment is available
- * @param {import("puppeteer").Page} page
- * @returns {Promise<string | null>}
+ * Gets the date and the booking url for which the appointment is available.
+ * If the slot is not available, returns null.
+ * @param {import("puppeteer").Page} page - The page to check for the slot.
+ * @returns {Promise<{date: string, url: string} | null>}
  */
-async function get_date(page) {
-	const td = await page.$("td.buchbar")
-	if (!td) return null
-	const link = await td.$("a")
-	if (!link) return null
-	const label = await page.evaluate((link) => link.getAttribute("aria-label"), link)
-	if (!label) return null
-	return label.split(" - ")?.[0] || null
+async function get_slot(page) {
+	return page.evaluate(() => {
+		const td = document.querySelector("td.buchbar")
+		if (!td) return null
+		const link = td.querySelector("a")
+		if (!link) return null
+		const date = link.getAttribute("title")?.split(" - ")[0]?.trim() ?? ""
+		const url = link.getAttribute("href") ?? ""
+		return { date, url }
+	})
 }
 
 /**
- * Sends an email to the recipient email address with a link to the booking page
- * @param {string | null} date The date for which the appointment is available
+ * Gets the time for which the appointment is available.
+ * If the time is not available, returns null.
+ * @param {import("puppeteer").Page} page - The page to check for the time.
+ * @returns {Promise<string | null>}
  */
-async function send_email(date) {
+async function get_time(page) {
+	return page.evaluate(() => {
+		/** @type {HTMLElement | null} */
+		const th = document.querySelector("th.buchbar")
+		if (!th) return null
+		return th.innerText.trim()
+	})
+}
+
+/**
+ * Sends an email to the recipient email address with a link to the booking page.
+ * @param {string} date The date for which the appointment is available.
+ * @param {string} time The time for which the appointment is available.
+ */
+async function send_email(date, time) {
 	const transporter = nodemailer.createTransport({
 		service: process.env.EMAIL_SERVICE,
 		auth: {
@@ -76,9 +122,7 @@ async function send_email(date) {
 		},
 	})
 
-	const subject = date
-		? `Termine für Personalausweis sind verfügbar am ${date}`
-		: "Termine für Personalausweis sind verfügbar"
+	const subject = `Termine verfügbar am ${date} um ${time} Uhr`
 
 	const options = {
 		from: process.env.EMAIL_USER,
@@ -96,4 +140,4 @@ async function send_email(date) {
 	})
 }
 
-main()
+check_appointments()
